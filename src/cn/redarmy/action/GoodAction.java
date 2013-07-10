0c2896951310b0234b739f3026904344f626baa7
alvin-xu <479359285@qq.com>
2013-07-10 15:55:25 +0800

    complete json transfer
    can receive json data with post function

--- src/cn/redarmy/action/GoodAction.java
+++ src/cn/redarmy/action/GoodAction.java
@@ -1,6 +1,14 @@
 package cn.redarmy.action;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import net.sf.json.JSONObject;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
 
 import cn.redarmy.domain.Good;
 import cn.redarmy.service.GoodService;
@@ -8,7 +16,7 @@ import cn.redarmy.service.impl.GoodServiceImpl;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class GoodAction extends ActionSupport{
+public class GoodAction extends ActionSupport implements ServletRequestAware{
 
 	/**
 	 * 
@@ -47,7 +55,45 @@ public class GoodAction extends ActionSupport{
     // 查询单个商品   
     public String findById() {   
         good = goodService.findGood();   
+        System.out.println("find by id");
         return SUCCESS;
-    }   
+    }
+
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		System.out.println("servlet request");
+		
+		if("get".equalsIgnoreCase(request.getMethod())){	//get 请求
+			//get请求的参数名若与属性名相同则框架会直接设置到属性值。
+			String result=request.getParameter("param");
+			System.out.println("result:"+result);
+			
+		}else{	//post请求
+			try {
+				BufferedReader reader=request.getReader();
+				String temp=null;
+				String result="";
+				
+				//用json格式通过post方法传递到服务器的内容与一般的post表单不一样，
+				//无法直接通过参数名获取
+				System.out.println("name:"+request.getParameter("name"));
+				System.out.println("age:"+request.getParameter("age"));
+				
+				while((temp=reader.readLine())!=null) result+=temp;
+				System.out.println("all:"+result);
+				
+				JSONObject json=JSONObject.fromObject(result);
+				
+				System.out.println("name:"+json.getString("name"));
+				System.out.println("age:"+json.getInt("age"));
+				System.out.println("gender:"+json.getString("gender"));
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
---
+++ src/cn/redarmy/action/MessageAction.java
@@ -0,0 +1,24 @@
+package cn.redarmy.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class MessageAction implements Action{
+
+	private String message;
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("message:"+message);
+		return SUCCESS;
+	}
+
+}
